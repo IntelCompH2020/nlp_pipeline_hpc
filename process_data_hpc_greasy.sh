6eb9e587fe05c0e2b30d1e5f5a18d798b6b9cc90
mapama247 <mpamies247@gmail.com>
2023-10-10 12:41:07 +0200

    update process_data_hpc bash scripts

--- process_data_hpc_greasy.sh
+++ process_data_hpc_greasy.sh
@@ -7,33 +7,33 @@ DATA_DIR=$1
 WORKING_DIR=$( pwd )
 
 for d in $DATA_DIR/*; do
-	BASENAME=$( basename $d )
-	if [ -d "$d" ]; then
-		if ! [[ -d $d"_nlp" ]]; then
-		  if test -f $d.yaml; then
-        # Create a text file with a list of tasks to be executed
-        bash hpc/greasy/create_tasks_file.sh $d
-        # Count the number of tasks to decide how many nodes to request
-        NTASKS=`wc -l < hpc/greasy/tasks/tasks_file_$BASENAME.txt`
-        # Specify the number of nodes (each one has 128 threads, plus one for the master)
-        NNODES=$(( ((NTASKS)/128)+1 ))
-        # Account for master
-        NTASKS=$(( NTASKS+1 ))
-        # Set the maximum number of tasks to 2048 (16 CTE-AMD nodes, 1 cpus per task)
-        NTASKS=$((NTASKS<2048 ? NTASKS : 2048))
-        # Edit the number of nodes requested in the SBATCH headers
-        sed --i -e "s/#SBATCH --ntasks=[0-9]\+/\#SBATCH --ntasks=$NTASKS/" hpc/greasy/greasy_scheduler.job
-        # Send job to queue using greasy
-        sbatch hpc/greasy/greasy_scheduler.job $WORKING_DIR/hpc/greasy/tasks/tasks_file_$BASENAME.txt
-        # Display some info about the job that has been sent
-        echo Job sent for: $BASENAME ($NTASKS tasks on $NNODES nodes)
-      else
-        echo Missing YAML file: $BASENAME
-      fi
-		else
-			echo Already processed: $BASENAME
-		fi
-	else
-		echo Not a directory: $BASENAME
-	fi
+    BASENAME=$( basename $d )
+    if [ -d "$d" ]; then
+        if ! [[ -d $d"_nlp" ]]; then
+            if test -f $d.yaml; then
+                # Create a text file with a list of tasks to be executed
+                bash hpc/greasy/create_tasks_file.sh $d
+                # Count the number of tasks to decide how many nodes to request
+                NTASKS=`wc -l < hpc/greasy/tasks/tasks_file_$BASENAME.txt`
+                # Account for master
+                NTASKS=$(( NTASKS+1 ))
+                # Set the maximum number of tasks to 2048 (16 CTE-AMD nodes, 1 cpus per task)
+                NTASKS=$((NTASKS<8 ? NTASKS : 8))
+                # Specify the number of nodes (each one has 128 threads, plus one for the master)
+#                 NNODES=$(( ((NTASKS)/128)+1 ))
+                # Edit the number of nodes requested in the SBATCH
+                sed --i -e "s/#SBATCH --ntasks=[0-9]\+/\#SBATCH --ntasks=$NTASKS/" hpc/greasy/greasy_scheduler_supp.job
+                    # Send job to queue using greasy
+                sbatch hpc/greasy/greasy_scheduler_supp.job $WORKING_DIR/hpc/greasy/tasks/tasks_file_$BASENAME.txt
+                # Display some info about the job that has been sent
+                echo Job sent for: $BASENAME. Running $NTASKS tasks on $NNODES nodes
+            else
+                echo Missing YAML file: $BASENAME
+            fi
+        else
+            echo Already processed: $BASENAME
+        fi
+    else
+        echo Not a directory: $BASENAME
+    fi
 done
--- process_data_hpc_sbatch.sh
+++ process_data_hpc_sbatch.sh
@@ -7,26 +7,26 @@ DATA_DIR=$1
 WORKING_DIR=$( pwd )
 
 for d in $DATA_DIR/*; do
-  BASENAME=$( basename $d )
-	if [ -d "$d" ]; then
-		if ! [[ "$d" =~ .*"_nlp".* ]]; then
-      if test -f $d.yaml; then
-        # Create a text file with a list of tasks to be executed
-        python hpc/sbatch/create_sbatchs.py --dir_path $d
-        # Iterate over bash scripts
-        for file in hpc/sbatch/$BASENAME/*.sh
-        do
-          # Send job to cluster
-          sbatch $file
-          echo Job sent for: $BASENAME
-        done
-      else
-        echo Missing YAML file: $BASENAME
-      fi
-		else
-			echo Already processed: $BASENAME
-		fi
-	else
-		echo Not a directory: $BASENAME
-	fi
+    BASENAME=$( basename $d )
+    if [ -d "$d" ]; then
+        if ! [[ -d $d"_nlp" ]]; then
+            if test -f $d.yaml; then
+                # Create a text file with a list of tasks to be executed
+                python hpc/sbatch/create_sbatchs.py --dir_path $d --num_gpus 365 
+                # Iterate over bash scripts
+                for file in hpc/sbatch/tasks/$BASENAME/*.sh
+                do
+                    # Send job to cluster
+                    sbatch $file
+                    echo Job sent for: $file
+                done
+            else
+                echo Missing YAML file: $BASENAME
+            fi
+        else
+            echo Already processed: $BASENAME
+        fi
+    else
+        echo Not a directory: $BASENAME
+    fi
 done
